eff9343fa41d697283a43e5838f418a0f11e6036
LidiaP <lidiaairinei@yahoo.com>
2023-03-08 19:06:21 +0200

    update5

--- Intalnirea 8 - Tema/ID_and_name_call.py
+++ Intalnirea 8 - Tema/ID_and_name_call.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 s = Service(ChromeDriverManager().install())
@@ -18,7 +20,8 @@ sleep(2)
 link_test = driver.find_element(By.NAME, 'firstname')
 link_test.send_keys('Lidia')
 driver.find_element(By.NAME, 'lastname').send_keys('Popa')
-driver.find_element(By.ID, 'sex-1').click()
+# add = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ezmob-footer-close]')))
+driver.find_element(By.ID, 'sex-0').click()
 driver.find_element(By.ID, 'exp-6').click()
 # driver.find_element(By.ID, 'submit').click()
 sleep(8)
